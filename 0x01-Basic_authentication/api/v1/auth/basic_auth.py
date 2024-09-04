0e955bd40b2af4f00dcce9d3c943f6173225174d
deezyfg <mensahpeter421@gmail.com>
2024-09-04 18:05:27 +0000

    Added/Updated Files

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,7 +52,7 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
-
+        return None
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
